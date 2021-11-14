eb351d43141732fb5b406f369540f10ae4c9038f
iTNTPiston <zhaoyizhu2000@gmail.com>
2021-11-13 23:52:56 -0800

    fix clean script

--- scripts/clean.py
+++ scripts/clean.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 from shutil import rmtree
 from os import path
-def clean(argv):
-	if path.exists("build"):
-		rmtree("build")
-	if path.exists("test"):
-		rmtree("test")
+
+if path.exists("build"):
+	rmtree("build")
+if path.exists("test"):
+	rmtree("test")
