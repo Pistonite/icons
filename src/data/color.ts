9dbe015d1c3e0cf66ccc4fa0e7fbac259faff65d
iTNTPiston <zhaoyizhu2000@gmail.com>
2023-06-01 12:43:12 -0700

    add totk icons

--- src/data/color.ts
+++ src/data/color.ts
@@ -21,6 +21,7 @@ export const Colors = {
     StandardOrange: defineColor("8c5e28", "ffffb5"),
     StandardGreen: defineColor("568c28", "c8e6af"),
     StandardPink: defineColor("d57b69", "fec1d3"),
+    StandardPurple: defineColor("644073", "b04beb"),
     StandardGray: defineColor("333333", "eeeeee"),
     MalicePurple: defineColor("f24c99", "ffc0fa"),
     DeepOrange: defineColor("8c5e28", "ff9900"),
@@ -30,6 +31,8 @@ export const Colors = {
     RudaniaRed: defineColor("d57b69", "fdfdfd"),
     LightGold: defineColor("cfa240", "ffd700"),
     DarkGold: defineColor("cfa240", "000000"),
+    UltraRed: defineColor("633830", "f0673a"),
+    FuseBlue: defineColor("2a767d", "59ffff"),
 } as const;
 
 export type ColorName = keyof typeof Colors;
@@ -65,6 +68,11 @@ export const Palettes = {
         frame: "StandardPink",
         center: "StandardPink",
         modifier: "StandardPink",
+    },
+    StandardPurple: {
+        frame: "StandardPurple",
+        center: "StandardPurple",
+        modifier: "StandardPurple",
     },
     StandardGray: {
         frame: "StandardGray",
@@ -110,7 +118,17 @@ export const Palettes = {
         frame: "DarkGold",
         center: "DarkGold",
         modifier: "DarkGold",
-    }
+    },
+    UltraRed: {
+        frame: "UltraRed",
+        center: "UltraRed",
+        modifier: "UltraRed",
+    },
+    FuseBlue: {
+        frame: "FuseBlue",
+        center: "FuseBlue",
+        modifier: "FuseBlue",
+    },
 } as const;
 
 export type PaletteName = keyof typeof Palettes;
